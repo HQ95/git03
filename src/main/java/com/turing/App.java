1328fad720769cfd4fa0b7436008772627e211f1
HQ <1211472735@qq.com>
2021-04-01 16:38:45 +0800

    合并

--- src/main/java/com/turing/App.java
+++ src/main/java/com/turing/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("main主要内容");
         System.out.println("说啥呢");
         System.out.println("dev主要内容");
     }
